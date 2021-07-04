4b2216840f5d267e96a45ba51985aba9ce5c00a1
whilgeek <whilgeek@gmail.com>
2021-07-04 18:15:05 +0800

    finally it worked

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -8,12 +8,23 @@ contract Election {
     }
 
     mapping(uint => Candidate) public candidates;
+    mapping(address => bool) public voters;
 
     uint public candidatesCount;
 
     function addCandidate(string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint _candidateID) public {
+        require(!voters[msg.sender]);
+
+        require(_candidateID > 0 && _candidateID <= candidatesCount);
+
+        voters[msg.sender] = true;
+
+        candidates[_candidateID].voteCount ++;
     }
 
     constructor() public {
--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,13 @@
 App = {
   web3Provider: null,
   contracts: {},
-  account: '0x0',
+  account: '0x8493Bb088E2f0A54d4B882E5BfC1961D1e05cF39',
 
-  init: function() {
+  init: function () {
     return App.initWeb3();
   },
 
-  initWeb3: function() {
+  initWeb3: function () {
     if (typeof web3 !== 'undefined') {
       // If a web3 instance is already provided by Meta Mask.
       App.web3Provider = web3.currentProvider;
@@ -20,8 +20,8 @@ App = {
     return App.initContract();
   },
 
-  initContract: function() {
-    $.getJSON("Election.json", function(election) {
+  initContract: function () {
+    $.getJSON("Election.json", function (election) {
       // Instantiate a new truffle contract from the artifact
       App.contracts.Election = TruffleContract(election);
       // Connect provider to interact with contract
@@ -31,7 +31,20 @@ App = {
     });
   },
 
-  render: function() {
+  castVote: function () {
+    var candidateId = $('#candidatesSelect').val();
+    App.contracts.Election.deployed().then(function (instance) {
+      return instance.vote(candidateId, { from: App.account });
+    }).then(function (result) {
+      // Wait for votes to update
+      $("#content").hide();
+      $("#loader").show();
+    }).catch(function (err) {
+      console.error(err);
+    });
+  },
+
+  render: function () {
     var electionInstance;
     var loader = $("#loader");
     var content = $("#content");
@@ -40,23 +53,29 @@ App = {
     content.hide();
 
     // Load account data
-    web3.eth.getCoinbase(function(err, account) {
+    web3.eth.getCoinbase(function (err, account) {
       if (err === null) {
+        console.log(account);
         App.account = account;
         $("#accountAddress").html("Your Account: " + account);
+      } else {
+        console.log(err);
       }
     });
 
     // Load contract data
-    App.contracts.Election.deployed().then(function(instance) {
+    App.contracts.Election.deployed().then(function (instance) {
       electionInstance = instance;
       return electionInstance.candidatesCount();
-    }).then(function(candidatesCount) {
+    }).then(function (candidatesCount) {
       var candidatesResults = $("#candidatesResults");
       candidatesResults.empty();
 
+      var candidatesSelect = $('#candidatesSelect');
+      candidatesSelect.empty();
+
       for (var i = 1; i <= candidatesCount; i++) {
-        electionInstance.candidates(i).then(function(candidate) {
+        electionInstance.candidates(i).then(function (candidate) {
           var id = candidate[0];
           var name = candidate[1];
           var voteCount = candidate[2];
@@ -64,19 +83,29 @@ App = {
           // Render candidate Result
           var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
           candidatesResults.append(candidateTemplate);
+
+          // Render candidate ballot option
+          var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
+          candidatesSelect.append(candidateOption);
         });
       }
-
+      console.log(App.account);
+      return electionInstance.voters(App.account);
+    }).then(function (hasVoted) {
+      // Do not allow a user to vote
+      if (hasVoted) {
+        $('form').hide();
+      }
       loader.hide();
       content.show();
-    }).catch(function(error) {
+    }).catch(function (error) {
       console.warn(error);
     });
-  }
-};
+  },
+}
 
-$(function() {
-  $(window).load(function() {
+$(function () {
+  $(window).load(function () {
     App.init();
   });
 });
